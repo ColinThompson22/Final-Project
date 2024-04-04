6d03b7e72bed5989b2ac7e9ea2f9adb05658180f
ibrhmoh <157778501+ibrhmoh@users.noreply.github.com>
2024-04-04 16:54:59 -0400

    Update collab_program.py

--- collab_program.py
+++ collab_program.py
@@ -1,2 +1,3 @@
 print("Hi my name is Colin Thompson and I am a memeber of Table 1")
 print("I'm Shahil and I'm also a member of Table 1!")
+print("My name is Mohammad and I am also a member of Table 1")
