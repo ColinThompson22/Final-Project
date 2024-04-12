375720f6929a2961a73fbe97b30052d0a8858dcf
ibrhmoh <157778501+ibrhmoh@users.noreply.github.com>
2024-04-12 18:30:06 -0400

    Update final_project_checkin.py

--- final_project_checkin.py
+++ final_project_checkin.py
@@ -6,12 +6,32 @@
 
 #question format function (Mohammad)
 def question_file(self):
-    quesiton_options:["Animal", "Height", "Weight", Color	Lifespan (years)	Diet	Habitat	Predators	Average Speed (km/h)	Countries Found	Conservation Status	Family	Gestation Period (days)	Top Speed (km/h)	Social Structure	Offspring per Birth]
-              math. 
-#allows computer to ask the user what certain characteristics are from the categories from the list
+    '''
+    Selects and formats a question from predefined categories based on the game state.
 
+    This method selects a random category from the list of question categories. If the number of questions asked
+    is below a certain threshold, it formats a question to ask the player.
+    
+    Returns:
+        None. The function prints a question to the console for the user to answer.
 
-#if self.num_questions_asked < or > blank/numerical then guess ra
+    '''
+   #List of categories the computer guesses from 
+    question_categories = ["Animal", "Height", "Weight", "Color", "Lifespan (years)", "Diet", "Habitat", "Predators", "Average Speed (km/h)", "Countries Found", "Conservation Status", "Family", "Gestation Period (days)", "Top Speed (km/h)", "Social Structure", "Offspring per Birth"]
+        
+
+    # Select a random characteristic to ask about
+    characteristic = random.choice(question_categories)
+
+    #Format and print the question
+    #Allows computer to ask the user what certain characteristics are from the categories from the list
+    question = f"What is the {question_options[random.choice]} of the animal you're thinking of?"
+    print(question)
+
+     #Determines how many quesitons have been asked before guessing 
+     self.num_questions_asked = 0
+     self.max_questions_before_guess = 10 
+
 
 
 #game state function (Colin)
