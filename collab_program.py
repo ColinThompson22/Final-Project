4234d62ca75960c1331e71012bd5d9b21188920c
ColinThompson22 <157647912+ColinThompson22@users.noreply.github.com>
2024-05-08 18:09:06 -0400

    Delete collab_program.py

--- collab_program.py
+++
@@ -1,5 +0,0 @@
-print("Hi my name is Colin Thompson and I am a memeber of Table 1")
-print("I'm Shahil and I'm also a member of Table 1!")
-print("My name is Mohammad and I am also a member of Table 1")
-print("Hi, I'm Shafiqat and I'm another member of Table 1")
-print("Hi, I'm Johnathan and I am a member of Table 1")
